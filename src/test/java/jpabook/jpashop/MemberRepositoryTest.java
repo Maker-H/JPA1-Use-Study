52f0032da912a82aae99b440fb1ef3053811c516
Heesom <heesom.hs@gmail.com>
2023-09-03 00:01:16 +0900

    [테스트] jpa 저장하고 조회한 후 저장한 객체와 조회한 객체가 같은지 테스트

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional // 테스트 케이스 후에 db 롤백해버리는 애노테이션
+    // @Rollback(false) 로 설정하면 데이터가 db에 들어감 (롤백하지 않고 커밋해버림)
+    public void testMember() throws Exception{
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isSameAs(member); // 저장한 것과 생성한 것이 같은지 확인
+
+        System.out.println("findMember = " + findMember);
+        System.out.println("member = " + member);
+    }
+}
