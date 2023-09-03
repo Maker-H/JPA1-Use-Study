cf9f650995638156f168089d9e37d6a440e395dd
Heesom <heesom.hs@gmail.com>
2023-09-03 21:41:09 +0900

    [기능] Member 엔티티와 레포지토리 추가
    - 멤버를 저장하는 기능
    - id를 기준으로 멤버를 조회하는 기능 추가

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // order 테이블에 있는 member를 기준으로 매핑됨
+    private List<Order> orders = new ArrayList<>();
+
+
+
+
+}
