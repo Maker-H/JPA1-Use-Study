eb608a8b9bc2557813ca1edd94e237ea7b3c6e61
Heesom <heesom.hs@gmail.com>
2023-09-03 21:42:20 +0900

    [테스트] Member에 대한 테스트 케이스 작성 (롤백 false, Transactional 애노테이션)
    - 멤버 객체를 저장후 조회해서 일치하는지 여부 확인

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
-
-import static org.junit.Assert.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
