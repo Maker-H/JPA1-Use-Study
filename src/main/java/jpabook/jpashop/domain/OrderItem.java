2f0c22f6abc512a3b76462aae5243b8c5e1685f3
진희솜 <heesom.hs@gmail.com>
2023-09-05 13:44:04 +0900

    [기능] OrderItem의 생성 메서드 생성
    - 관계에 있는 item에 대한 것만 설정해주고 order는 Order쪽에서 생성할때 설정해준다. (주문을 하는 주체는 Order이기 때문)
    - item에 대한 재고도 감산해준다

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -31,8 +34,29 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    //==비즈니스 로직==//
+    //==생성 메서드==//
 
+    /**
+     *
+     * 1. 관계에 있는 Item을 받은 다음 관계 설정
+     * (Order을 넣지 않는 이유는 OrderItem은 Order이 생성될 때만 생성되기에 Order에서 설정해주는 것이 맞기 때문이다.)
+     *
+     * 2. item의 재고 감산
+     *
+     * = 주문 생성 => 주문에 따른 주문아이템 생성
+     * = 주문 아이템 생성
+     */
+    public static OrderItem orderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // item에 대한 재고 설정
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
     /**
      * 주문항목을 취소하면 그에 대한 재고가 다시 item으로 들어가야한다
      */
